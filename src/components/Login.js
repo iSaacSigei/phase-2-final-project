d64b44747366d943f18c168cae17b896e5ad02ac
iSaacSigei <kiplangatisack.is@gmail.com>
2022-10-03 13:02:15 +0300

    initial commit

---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div>
+      <form>
+        <input type="email" placeholder="enter student email" name="email" />
+      </form>
+    </div>
+  );
+}
+
+export default Login;
