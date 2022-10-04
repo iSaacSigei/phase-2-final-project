7bb72a18ee1dc1fa98ec5218349bf1da3d576892
iSaacSigei <kiplangatisack.is@gmail.com>
2022-10-04 15:28:58 +0300

    added the navbar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,61 @@
-import React from 'react'
-import {NavLink} from 'react-router-dom'
+import React from "react";
+import { NavLink, useHistory } from "react-router-dom";
 
-function NavBar() {
+function NavBar({ setIsLoggedIn }) {
+  const history = useHistory();
+  function handleLogout() {
+    setIsLoggedIn(false);
+    history.push("/login");
+  }
+
+  const linkStyles = {
+    width: "150px",
+    height:"30px",
+    paddingTop: "10px",
+    borderRadius:"8px",
+    padding:"2px",
+    alignItems:"center",
+    margin: "0 6px 6px",
+    background: "blue",
+    textDecoration: "none",
+    color: "white",
+  };
+
   return (
-    <div className='navbar'>
-        <NavLink to="/">Home</NavLink>
-
+    <div className="navbar">
+      <NavLink
+        to="/"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/about"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        About
+      </NavLink>
+      <NavLink
+        to="/login"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Login
+      </NavLink>
+      <button onClick={handleLogout}>Logout</button>
     </div>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
