efbea0406c25bfb450b1d722a2371a3efadf2325
iSaacSigei <kiplangatisack.is@gmail.com>
2022-10-04 15:28:39 +0300

    added the login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
-function Login() {
+function Login({ setIsLoggedIn }) {
+  const history = useHistory();
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  function handleChange(e) {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setIsLoggedIn(true);
+    history.push("/");
+  }
+
   return (
-    <div>
-      <form>
-        <input type="email" placeholder="enter student email" name="email" />
-      </form>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <h1>Login</h1>
+      <input
+        type="text"
+        name="username"
+        value={formData.username}
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        name="password"
+        value={formData.password}
+        onChange={handleChange}
+      />
+      <button type="submit">Login</button>
+    </form>
   );
 }
 
