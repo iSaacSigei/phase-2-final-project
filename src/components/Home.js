1ea7794e581103920f94454244a89a7e9a562420
iSaacSigei <kiplangatisack.is@gmail.com>
2022-10-06 08:26:12 +0300

    render the fetched data on the Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { Redirect } from "react-router-dom";
 
-function Home({ isLoggedIn }) {
+function Home({ isLoggedIn, notes }) {
+  console.log(notes);
+  const noteCard = notes.map((note) => (
+    <>
+      <div className="imgContainer">
+        <img src={note.url} alt="Oops" />
+        <h3>{note.title}</h3>
+        <p>{note.message}</p>
+        <h5>@{note.author}</h5>
+      </div>
+    </>
+  ));
   if (!isLoggedIn) return <Redirect to="/login" />;
 
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
+  return <div className="imageCard">{noteCard}</div>;
 }
 
 export default Home;
