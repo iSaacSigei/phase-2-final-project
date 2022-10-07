f47d3580051b4ceb18b8d12141e4be967bb9d9c9
iSaacSigei <kiplangatisack.is@gmail.com>
2022-10-07 09:09:02 +0300

    add useHsitoy to redirect to home page when form is submitted

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,21 +1,97 @@
-import React from "react";
-import { Redirect } from "react-router-dom";
+import React, { useState } from "react";
+import { Redirect,useHistory } from "react-router-dom";
 
+function Notes({ isLoggedIn ,handleNewData}) {
+  const[success, setSuccess]=useState(false)
+  const history = useHistory();
+  const[newNote, setNewNote]=useState({
+    title:"",
+    author:"",
+    url:"",
+    message:""
+  })
 
-function Notes({isLoggedIn}) {
+  function handleChange(e) {
+    console.log(e.target.value);
+    let name=e.target.name
+    let value=e.target.value
+    setNewNote({
+      ...newNote, [name]:value
+    })
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch('http://localhost:3000/notes',{
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify(newNote)
+    })
+    .then(res=>res.json())
+    .then(data=>{
+      console.log(data)
+    })
+    handleNewData()
+    setSuccess(true)
+    history.push("/")
+  }
   if (!isLoggedIn) return <Redirect to="/login" />;
-  return(
-    <div className="notes"x>
-        <form >
-          <label>Title</label>
-          <input type="text" placeholder="Note Title" />
-          <label>category</label>
-          <input type="text" placeholder="Note Category"/>
-          <label>Note</label>
-          <textarea name="notes" required placeholder="write your note here" maxLength="200"></textarea>
-        </form>
+  return (
+    <>
+    <div>
+      <form onSubmit={handleSubmit} className="notes">
+        <label>
+          Title :{" "}
+          <input
+            onChange={handleChange}
+            className="formInput"
+            type="text"
+            placeholder="Note Title"
+            name="title"
+          />
+        </label>
+
+        <label>
+          Author :{" "}
+          <input
+            onChange={handleChange}
+            className="formInput"
+            type="text"
+            placeholder="Author's Name"
+            name="author"
+          />
+        </label>
+
+        <label>
+          Image url :{" "}
+          <input
+            onChange={handleChange}
+            className="formInput"
+            type="url"
+            placeholder="enter image url"
+            name="url"
+          />
+        </label>
+
+        <label className="text">Note</label>
+        <textarea
+          onChange={handleChange}
+          id="note"
+          name="message"
+          placeholder="write your note here"
+          maxLength="200"
+          required
+        ></textarea>
+        <input
+          style={{ width: "100px", marginTop: "30px", marginLeft: "280px" }}
+          type="submit"
+          value="Add to Notes"
+        />
+      </form>
     </div>
-  )
+    </>
+  );
 }
 
 export default Notes;
