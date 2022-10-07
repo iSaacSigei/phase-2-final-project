4593d09a0f7b4a86fb72325868a8e724cfd31f79
iSaacSigei <kiplangatisack.is@gmail.com>
2022-10-07 09:10:57 +0300

    added hook state to search through the notes

--- src/components/App.js
+++ src/components/App.js
@@ -7,31 +7,36 @@ import Notes from "./Notes";
 import Home from "./Home";
 
 function App() {
-
-const[notes, setNotes]=useState([])
-useEffect(()=>{
-  fetch('http://localhost:3000/notes')
-  .then(res=>res.json())
-  .then((data)=>{
-    console.log(data)
-    setNotes(data)
-    
-  })
-},[])
+  const [notes, setNotes] = useState([]);
+  const [newData, setNewData] = useState(false);
+  const [search, setSearch] = useState("");
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    fetch("http://localhost:3000/notes")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setNotes(data);
+      });
+  }, [newData]);
+
+  function handleNewData() {
+    setNewData((newData) => !newData);
+  }
+
   return (
     <div className="App">
-      <NavBar setIsLoggedIn={setIsLoggedIn} />
-      <hr/>
+      <NavBar setIsLoggedIn={setIsLoggedIn} setSearch={setSearch} />
+      <hr />
       <Switch>
         <Route exact path="/notes">
-          <Notes isLoggedIn={isLoggedIn}/>
+          <Notes isLoggedIn={isLoggedIn} handleNewData={handleNewData} />
         </Route>
         <Route exact path="/login">
           <Login setIsLoggedIn={setIsLoggedIn} />
         </Route>
         <Route exact path="/">
-          <Home isLoggedIn={isLoggedIn} notes={notes}/>
+          <Home isLoggedIn={isLoggedIn} notes={notes} search={search} />
         </Route>
       </Switch>
     </div>
