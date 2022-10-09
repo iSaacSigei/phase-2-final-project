42279f8fc43f1263b6ef9ba741d3a0f46e393260
iSaacSigei <kiplangatisack.is@gmail.com>
2022-10-09 14:54:06 +0300

    mode the rendering to the NotesList component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,31 @@
 import React from "react";
-import { Redirect } from "react-router-dom";
+import { Redirect, useHistory } from "react-router-dom";
+import NotesList from "./NotesList";
 
-function Home({ isLoggedIn, notes, search }) {
-  const filteredNotes = notes.filter((eachNote) =>
-    eachNote.message.toLowerCase().includes(search.toLowerCase())
-  ).map((note) => (
-    <>
-      <div className="imgContainer">
-        <img src={note.url} alt="Oops" />
-        <h3>{note.title}</h3>
-        <p>{note.message}</p>
-        <h5>@{note.author}</h5>
-      </div>
-    </>
-  ));
+function Home({ isLoggedIn, notes, search, handleDelete }) {
+  const history = useHistory();
+  function handleClick() {
+    history.push("/notes");
+  }
+
   if (!isLoggedIn) return <Redirect to="/login" />;
 
-  return <div className="imageCard">{filteredNotes}</div>;
+  return (
+    <>
+      <div className="imageCard">
+        {notes
+          .filter((eachNote) =>
+            eachNote.message.toLowerCase().includes(search.toLowerCase())
+          )
+          .map((note) => (
+            <NotesList key={note.id} id={note.id} note={note} handleDelete={handleDelete} />
+          ))}
+      </div>
+      <button onClick={handleClick} style={{ marginTop: "100px",height:"30px" }}>
+        Add New Note
+      </button>
+    </>
+  );
 }
 
 export default Home;
